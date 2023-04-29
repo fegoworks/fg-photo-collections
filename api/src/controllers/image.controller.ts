8f44b8afb9af3fd013fc51b6247d5d2261449861
fegoworks <fegoworks@hotmail.com>
2023-04-29 06:39:06 +0100

    feature(controllers): build out image controller

---
+++ api/src/controllers/image.controller.ts
@@ -0,0 +1,58 @@
+import type { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { Images } from "../entities/image.entity";
+import logger from "../loggers/logger";
+import uploadService from "../services/upload.service";
+import { BadRequestError, NotFoundError } from "../utils/ApiError";
+import { asyncWrapper } from "../utils/AsyncWrapper";
+
+class ImageController {
+  saveImage = asyncWrapper(async (req: Request, res: Response) => {
+    const file = req.file;
+    if (!file) {
+      throw new BadRequestError("Error: File is required");
+    }
+    const image = new Images();
+    const data = await uploadService.uploadSingleToCloudinary({
+      file,
+      opts: {
+        folder: "image",
+      },
+    });
+
+    if (!data) {
+      throw new BadRequestError("Error uploading image");
+    }
+
+    image.image_url = data?.secure_url;
+    image.created_at = new Date();
+    const savedImage = await image.save();
+
+    if (!savedImage) {
+      throw new BadRequestError("Error saving image");
+    }
+
+    logger.info("POST request made for images API.");
+    return res.status(StatusCodes.OK).send({
+      message: "successful",
+      image_url: savedImage.image_url,
+    });
+  });
+
+  getImage = asyncWrapper(async (req: Request, res: Response) => {
+    const images = await Images.find();
+    if (!images) {
+      throw new NotFoundError("Images not found");
+    }
+
+    logger.info("GET request made for images API.");
+    return res.status(StatusCodes.OK).send({
+      message: "successful",
+      images,
+    });
+  });
+}
+
+const imageController = new ImageController();
+
+export default imageController;
